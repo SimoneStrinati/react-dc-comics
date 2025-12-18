8d8f8f678701589a5d0c46d7a35ef0725c89ec90
simonestrinati <simone.strinati@outlook.it>
2025-12-18 21:44:11 +0100

    creazione footer e suo CSS

--- src/components/BlueBandSection.jsx
+++ src/components/BlueBandSection.jsx
@@ -1,28 +1,28 @@
 function BlueBandSection() {
     return (
-        <section class="blue-band">
-            <div class="blue-container">
-                <div class="blue-item">
+        <section className="blue-band">
+            <div className="blue-container">
+                <div className="blue-item">
                     <img src="/buy-comics-digital-comics.png" alt="Digital Comics" />
                     <span>Digital Comics</span>
                 </div>
 
-                <div class="blue-item">
+                <div className="blue-item">
                     <img src="/buy-comics-merchandise.png" alt="Merchandise" />
                     <span>DC Merchandise</span>
                 </div>
 
-                <div class="blue-item">
+                <div className="blue-item">
                     <img src="/buy-comics-subscriptions.png" alt="Subscritions" />
                     <span>Subscription</span>
                 </div>
 
-                <div class="blue-item">
+                <div className="blue-item">
                     <img src="/buy-comics-shop-locator.png" alt="Comics Shop" />
                     <span>Comic Shop Locator</span>
                 </div>
 
-                <div class="blue-item">
+                <div className="blue-item">
                     <img src="/buy-dc-power-visa.svg" alt="Power Visa" />
                     <span>DC Power Visa</span>
                 </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,64 @@
-function Footer () {
+function Footer() {
     return (
-        <footer>Footer</footer>
+        <footer className="footer-top">
+            <div className="footer-container">
+
+                <div className="footer-links">
+                    <div>
+                        <h4>DC Comics</h4>
+                        <ul>
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Videos</li>
+                            <li>News</li>
+                        </ul>
+
+                        <h4>Shop</h4>
+                        <ul>
+                            <li>Shop DC</li>
+                            <li>Shop DC Collectibles</li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h4>DC</h4>
+                        <ul>
+                            <li>Terms Of Use</li>
+                            <li>Privacy Policy</li>
+                            <li>Ad Choices</li>
+                            <li>Advertising</li>
+                            <li>Jobs</li>
+                            <li>Subscriptions</li>
+                            <li>Talent Workshops</li>
+                            <li>CPSC Certificates</li>
+                            <li>Ratings</li>
+                            <li>Shop Help</li>
+                            <li>Contact Us</li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h4>Sites</h4>
+                        <ul>
+                            <li>DC</li>
+                            <li>MAD Magazine</li>
+                            <li>DC Kids</li>
+                            <li>DC Universe</li>
+                            <li>DC Power Visa</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div className="footer-logo">
+                    <img src="/dc-logo-bg.png" alt="DC Logo"/>
+                </div>
+
+            </div>
+        </footer>
+
     )
 }
 
