bf48a5cc9695ac8bcabb1ae6a55b345859c26db2
simonestrinati <simone.strinati@outlook.it>
2025-12-18 20:59:53 +0100

    Creazione fascia blu con CSS

--- src/components/BlueBandSection.jsx
+++ src/components/BlueBandSection.jsx
@@ -1,7 +1,32 @@
 function BlueBandSection() {
     return (
-        <section>
-            <h2>contenuto qui</h2>
+        <section class="blue-band">
+            <div class="blue-container">
+                <div class="blue-item">
+                    <img src="/buy-comics-digital-comics.png" alt="Digital Comics" />
+                    <span>Digital Comics</span>
+                </div>
+
+                <div class="blue-item">
+                    <img src="/buy-comics-merchandise.png" alt="Merchandise" />
+                    <span>DC Merchandise</span>
+                </div>
+
+                <div class="blue-item">
+                    <img src="/buy-comics-subscriptions.png" alt="Subscritions" />
+                    <span>Subscription</span>
+                </div>
+
+                <div class="blue-item">
+                    <img src="/buy-comics-shop-locator.png" alt="Comics Shop" />
+                    <span>Comic Shop Locator</span>
+                </div>
+
+                <div class="blue-item">
+                    <img src="/buy-dc-power-visa.svg" alt="Power Visa" />
+                    <span>DC Power Visa</span>
+                </div>
+            </div>
         </section>
     );
 }
