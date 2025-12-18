451e9efbb475edf88661385999c138234a0d08d2
simonestrinati <simone.strinati@outlook.it>
2025-12-18 20:45:34 +0100

    stilizzazione Header e suo CSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,26 @@
-function Header () {
+function Header() {
     return (
-        <header>Header</header>
+        <header>
+
+            <div className="flex-container">
+                <img id="logo" src="/dc-logo.png" alt="Logo DC Comics" />
+
+                <nav>
+                    <ul>
+                        <li><a href="">Characters</a></li>
+                        <li><a href="">Comics</a></li>
+                        <li><a href="">Movies</a></li>
+                        <li><a href="">TV</a></li>
+                        <li><a href="">Games</a></li>
+                        <li><a href="">Collectibles</a></li>
+                        <li><a href="">Videos</a></li>
+                        <li><a href="">Fans</a></li>
+                        <li><a href="">News</a></li>
+                        <li><a href="">Shop</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
     )
 }
 
