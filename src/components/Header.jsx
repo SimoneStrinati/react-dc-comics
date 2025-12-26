ea574d4f7d19bc651ed1787758b3dcad1f591a03
simonestrinati <simone.strinati@outlook.it>
2025-12-26 23:11:41 +0100

    navbar dinamica usando .map

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,51 @@
 import style from "./Header.module.css";
 
+const links = [
+    { 
+        id:1,
+        text: "Characters"
+    },
+    { 
+        id:2,
+        text: "Comics"
+    },
+    { 
+        id:3,
+        text: "Movies"
+    },
+    { 
+        id:4,
+        text: "TV"
+    },
+    { 
+        id:5,
+        text: "Games"
+    },
+    { 
+        id:6,
+        text: "Collectibles"
+    },
+    { 
+        id:7,
+        text: "Videos"
+    },
+    { 
+        id:8,
+        text: "Fans"
+    },
+    { 
+        id:9,
+        text: "News"
+    },
+    { 
+        id:10,
+        text: "Shop"
+    }
+]
+
 function Header() {
     return (
+
         <header className={style.header}>
 
             <div className={style.headerFlex}>
@@ -9,7 +53,7 @@ function Header() {
 
                 <nav>
                     <ul>
-                        <li><a href="">Characters</a></li>
+                        {/* <li><a href="">Characters</a></li>
                         <li><a href="">Comics</a></li>
                         <li><a href="">Movies</a></li>
                         <li><a href="">TV</a></li>
@@ -18,7 +62,11 @@ function Header() {
                         <li><a href="">Videos</a></li>
                         <li><a href="">Fans</a></li>
                         <li><a href="">News</a></li>
-                        <li><a href="">Shop</a></li>
+                        <li><a href="">Shop</a></li> */}
+                        {
+                        links.map((link, i)=> {
+                        return <li key={i}><a href="">{link.text}</a></li>})
+                        }
                     </ul>
                 </nav>
             </div>
