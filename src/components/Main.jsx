b3887c8575803437ae64a44c311bd108eb56e1f9
simonestrinati <simone.strinati@outlook.it>
2026-01-01 21:12:29 +0100

    creazione struttura card e relativo CSS

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,20 +1,28 @@
 import style from "./Main.module.css";
-import comics from "../data/comics.js"; 
+import comics from "../data/comics.js";
 
 function Main() {
     return (
         <main className={style.main}>
-            <div>
+            <div className={style.comicBox}>
                 {
                     comics.map((comic) => {
-                        return <div>
-                            <img src={comic.thumb} alt="immagine fumetto" />
+                        return <div className={style.card}>
+
+                            <img src={comic.thumb} alt={comic.description} />
                             <h4>{comic.title}</h4>
-                            
-                        </div> 
+
+                        </div>
+
                     })
                 }
+
             </div>
+
+            <div className={style.btnFlex}>
+                <button className={style.loadButton}>LOAD MORE</button>
+            </div>
+
         </main>
     );
 }
