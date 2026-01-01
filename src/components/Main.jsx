8a64b9b672608766af4a939a39b15a11e9e6d0d4
simonestrinati <simone.strinati@outlook.it>
2026-01-01 19:47:46 +0100

    import di comics.js e creazione ciclo map

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,20 @@
 import style from "./Main.module.css";
+import comics from "../data/comics.js"; 
 
 function Main() {
     return (
         <main className={style.main}>
-            <h2>--Content goes here--</h2>
+            <div>
+                {
+                    comics.map((comic) => {
+                        return <div>
+                            <img src={comic.thumb} alt="immagine fumetto" />
+                            <h4>{comic.title}</h4>
+                            
+                        </div> 
+                    })
+                }
+            </div>
         </main>
     );
 }
