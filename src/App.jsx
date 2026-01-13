98dc938adf36fcb6bcdf780e09a4932105be5b7b
simonestrinati <simone.strinati@outlook.it>
2026-01-13 21:17:02 +0100

    aggiunta Jumbotron e relativo CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./components/Header"
+import Jumbotron from "./components/Jumbotron"
 import Main from "./components/Main"
 import BlueBandSection from "./components/BlueBandSection"
 import Footer from "./components/Footer"
@@ -9,6 +10,7 @@ function App() {
     <>
       <div id="app">
         <Header />
+        <Jumbotron />
         <Main />
         <BlueBandSection />
         <Footer />
---
+++ src/components/Jumbotron.jsx
@@ -0,0 +1,9 @@
+function Jumbotron() {
+    return (
+        <div id="jumbotron"> 
+            <img src="/jumbotron.jpg" alt="" />
+        </div>
+    )
+}
+
+export default Jumbotron; 
